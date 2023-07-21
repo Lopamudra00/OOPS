5229eeab3fca7ad1a43b3f7b47826f08ec6a0c97
Lopamudra Mallick <88570040+Lopamudra00@users.noreply.github.com>
2023-07-21 23:48:19 +0530

    pointer passed as an argument

--- pointer.cpp
+++ pointer.cpp
@@ -43,5 +43,14 @@ int main(){
     cout << "Addition of comp1,comp2" << endl;
     comp3 = add2Number(comp1, comp2);
     comp3.displayData();
+
+    cout << "Pointer to object" << endl;
+    ComplexNumber *ptr1; // Here ComplexNumber is acting as a datatype for pointer ptr1;
+    ptr1 = &comp3; //now this pointer can be point to comp3 since this comp3 is a type of ComplexNumber
+    ptr1->displayData();
+
+    ptr1 = &comp2;
+    ptr1->displayData();
+    cout << ptr1;
     return 0;
 }
